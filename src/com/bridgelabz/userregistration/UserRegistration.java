4b64a9a25fad1de1825ff09e5aa7218e9e6ff8c4
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-27 20:58:59 +0530

    [kshitija]:uscase 2 for user lastname is solved

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -4,8 +4,11 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 	String regex="[A-Z]{1}[a-z]{2,9}";
-	String str="Kshitija";
-	System.out.println(str.matches(regex));
+	String firstname="Kshitija";
+	System.out.println(firstname.matches(regex));
+	String regex1="[A-Z]{1}[a-z]{2,9}";
+	String lastname="Patil";
+	System.out.println(lastname.matches(regex1));
 	}
 
 }
