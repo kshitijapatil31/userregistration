89c817b1b97a59a6c2e3c5d931b3017fc1619792
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-27 21:10:52 +0530

    [kshitija]:usecase 3 for email validation is solved

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -9,6 +9,10 @@ public class UserRegistration {
 	String regex1="[A-Z]{1}[a-z]{2,9}";
 	String lastname="Patil";
 	System.out.println(lastname.matches(regex1));
+	String regex2="^[a-z]{3}(.+)[a-zA-z_+-]{0,8}@([a-zA-Z]{3,14}(.+))([a-zA-z]{2})$";
+	String email="abc.xy_z@Bridgelabz.co";
+	
+	System.out.println(email.matches(regex));
 	}
 
 }
